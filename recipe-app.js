e135bf6591b83bb43b2fb8fa53f82f4532fcd2c6
Vatsal <ramivatsal07@gmail.com>
2020-01-03 00:16:11 +0530

    Added a search functionality in the application

--- recipe-app.js
+++ recipe-app.js
@@ -1,1 +1,34 @@
-console.log('This is working')
+const recipes = [{
+    title: 'Aloo Paratha'
+},{
+    title: 'Pizza'
+},{
+    title: 'Sandwich'
+},{
+    title: 'Burger'
+},{
+    title: 'Vadapav'
+}]
+
+const filters = {
+   searchText: '' 
+}
+
+const renderRecipes = (recipes, filters) => {
+    const filteredRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(filters.searchText.toLowerCase()))
+    
+    document.querySelector('#recipes').innerHTML = ''
+
+    filteredRecipes.forEach(recipe => {
+        const recipeEl = document.createElement('p')
+        recipeEl.textContent = recipe.title
+        document.querySelector('#recipes').appendChild(recipeEl)
+    });
+}
+
+renderRecipes(recipes, filters)
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderRecipes(recipes, filters)
+})
