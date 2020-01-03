c5142d1d2214c5b7b1b4de55d25e860584b2a816
Vatsal <ramivatsal07@gmail.com>
2020-01-03 17:02:13 +0530

    added styling to webpage

--- recipe-functions.js
+++ recipe-functions.js
@@ -26,39 +26,49 @@ const removeRecipe = (id) => {
 
 // Generating DOM structure for a recipe
 const generateRecipeDOM = (recipe) => {
-    const recipeEl = document.createElement('div')
-    const textEl = document.createElement('a')
-    const button = document.createElement('button')
+    const recipeEl = document.createElement('a')
+    const textEl = document.createElement('p')
+    const ingredientsEl = document.createElement('p')
 
-    button.textContent = 'Remove'
-    recipeEl.appendChild(button)
-    button.addEventListener('click',(e) => {
-        removeRecipe(recipe.id)
-        saveRecipes(recipes)
-        renderRecipes(recipes, filters)
-    })
-
+    // Setup the recipe title text
     if (recipe.title.length > 0) {
         textEl.textContent = recipe.title
     } else {
         textEl.textContent = 'Unnamed Recipe'
     }
-
-    textEl.setAttribute('href', `/edit.html#${recipe.id}`)
-
+    textEl.classList.add('list-item__title')
     recipeEl.appendChild(textEl)
+
+    // Setup the link
+    recipeEl.setAttribute('href', `/edit.html#${recipe.id}`)
+    recipeEl.classList.add('list-item')
+
+    // Setup the ingredients text
+    if (recipe.ingredients.length > 0) {
+        ingredientsEl.textContent = recipe.ingredients
+    } else {
+        ingredientsEl.textContent = 'Add ingredients'
+    }
+    ingredientsEl.classList.add('list-item__subtitle')
+    recipeEl.appendChild(ingredientsEl)
 
     return recipeEl
 }
 
 // Render application recipes
 const renderRecipes = (recipes, filters) => {
-    const filteredRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(filters.searchText.toLowerCase()))
-    
+    const filteredRecipes = recipes.filter(recipe => recipe.title.toLowerCase().includes(filters.searchText.toLowerCase())) 
     document.querySelector('#recipes').innerHTML = ''
 
-    filteredRecipes.forEach(recipe => {
-        const recipeEl = generateRecipeDOM(recipe)
-        document.querySelector('#recipes').appendChild(recipeEl)
-    });
+    if (filteredRecipes.length > 0) {
+        filteredRecipes.forEach(recipe => {
+            const recipeEl = generateRecipeDOM(recipe)
+            document.querySelector('#recipes').appendChild(recipeEl)
+        })
+    } else {
+        const message = document.createElement('h2')
+        message.textContent = 'You do not have any recipes to show'
+        message.classList.add('empty-message')
+        document.querySelector('#recipes').appendChild(message)
+    }
 }
